0d7fdfdedf9919bb254810666b2c84c616880e16
Paul Bunyan <hal7df@gmail.com>
2023-07-02 19:16:12 -0400

    Remove unused imports

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -6,13 +6,11 @@ pub mod io;
 pub mod task;
 
 use log::warn;
-use tokio::sync::watch::Receiver as WatchReceiver;
-use tokio::sync::oneshot::Receiver as OneshotReceiver;
-use tokio::sync::oneshot::error::TryRecvError;
+use tokio::sync::watch::Receiver;
 
-///! Checks whether the given [`WatchReceiver`] for application shutdown has
+///! Checks whether the given [`Receiver`] for application shutdown has
 ///! been issued a shutdown notification without `await`ing.
-pub fn check_shutdown_rx(rx: &mut WatchReceiver<bool>) -> bool {
+pub fn check_shutdown_rx(rx: &mut Receiver<bool>) -> bool {
     if let Ok(has_changed) = rx.has_changed() {
         !has_changed || *rx.borrow_and_update()
     } else {
